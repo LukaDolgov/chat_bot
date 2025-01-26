40b658b18e0ed80e3412e8d4386e18334a4fb3e0
LukaDolgov <luka.dolgov@gmail.com>
2025-01-26 12:26:52 -0500

    sir kawgit's liscence

--- TrainerLuka.py
+++ TrainerLuka.py
@@ -3,7 +3,16 @@ import wandb
 import torch
 from settings import dataset_name, max_seq_size, vocab_size
 
-
+#    Copyright 2025 Kenneth Wilber (kawgit)
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 NEG_INFINITY = 1e-9
